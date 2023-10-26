3d0224689751c1c9188c8524f9bf23abe3ee518e
SGuevaraOlie <santiguevara98@gmail.com>
2023-10-26 19:58:09 -0300

    models listos¿

---
+++ cr-pi-countries-main/server/src/models/Activity.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Activity', {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dificultad:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1,
+                max: 5
+            }
+        },
+        duracion:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        temporada:{
+            type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera'),
+            allowNull: false
+        }
+})
+};
--- cr-pi-countries-main/server/src/models/Country.js
+++ cr-pi-countries-main/server/src/models/Country.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Country', {
+    id:{
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    image:{
+       type: DataTypes.STRING,
+       allowNull: false,
+    },
+    continente:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subergion:{
+      type: DataTypes.STRING,
+    },
+    area:{
+      type: DataTypes.INTEGER,
+    },
+    poblacion:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+})
 };
