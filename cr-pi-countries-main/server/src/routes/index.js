9b86e6613dfae0689f12864d133262f2eae8f835
SGuevaraOlie <santiguevara98@gmail.com>
2023-11-01 00:12:48 -0300

    rutas listas

--- cr-pi-countries-main/server/src/routes/index.js
+++ cr-pi-countries-main/server/src/routes/index.js
@@ -1,5 +1,19 @@
 const { Router } = require("express");
+// Activities
+const getActivities = require('../controllers/activities/getActivities')
+const postActivities = require('../controllers/activities/postActivities')
+// Countries
+const getCountries = require('../controllers/countries/getCountries')
+const getCountriesById = require('../controllers/countries/getCountriesById')
+const getCountriesByName = require('../controllers/countries/getCountriesByName')
 
 const router = Router();
+
+router.get('/countries', getCountries)
+router.get('/countries/:idPais', getCountriesById)
+router.get('/countries/name?="..."', getCountriesByName)
+
+router.get('/activities', getActivities)
+router.post('/activities', postActivities)
 
 module.exports = router;
